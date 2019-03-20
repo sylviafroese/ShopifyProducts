0da528e53e7c9cb0e58b491839531c38202a681e
Sylvia Froese <slfroese@mymts.net>
2019-03-20 15:08:50 -0500

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Mvc;
+
+
+
+namespace SylviaShopifyWebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        static HttpClient client = new HttpClient();
+
+        // This method will get all the products in the store
+       [HttpGet]
+       static void GetProducts()
+        {
+            // connecting to the Shopify store using the Product resource
+            dynamic shopify = new Shopify.Api("https://your_store.com/admin/products", "myAPIkey", "myAPIpassword");
+
+            // create a query that pulls all the information in JSON format
+            dynamic allProductsQuery = shopify.Products();
+
+            // writes the product title, product price and product image name to the console.
+            foreach (dynamic product in allProductsQuery.products)
+            {
+                Console.WriteLine($"{product.title}\t{product.price}\t{product.images}");
+            }
+        }
+
+
+        // This method will update the product price and description given the specific product_id passed to the method.
+        [HttpPut]
+        static void UpdateProducts(string product_id, double price, string description)
+        {
+            // connecting to the Shopify store using the Product resource to update a product
+            dynamic shopifyUpdate = new Shopify.Api("https://yourstore.com/admin/products/#{product_id}", "myAPIkey", "myAPIpassword");
+
+            // create a query that pulls the information for a specific product.
+            dynamic specificProductQuery = shopifyUpdate.Products();
+
+            // update the price and description given the price and description that was passed to the method
+            specificProductQuery.price = price;
+            specificProductQuery.description = description;
+
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace GetShopifyData
+{
+    public class Variant
+    {
+        public Variant()
+        {
+        }
+
+        public Variant(object id, object product_id, string title, string price, string sku, string option1, string option2, int inventory_quantity)
+        {
+            this.id = id;
+            this.product_id = product_id;
+            this.title = title;
+            this.price = price;
+            this.sku = sku;
+            this.option1 = option1;
+            this.option2 = option2;
+            this.inventory_quantity = inventory_quantity;
+
+        }
+
+        public object id { get; set; }
+        public object product_id { get; set; }
+        public string title { get; set; }
+        public string price { get; set; }
+        public string sku { get; set; }
+        public int position { get; set; }
+        public int? grams { get; set; }
+        public string inventory_policy { get; set; }
+        public object compare_at_price { get; set; }
+        public string fulfillment_service { get; set; }
+        public string inventory_management { get; set; }
+        public string option1 { get; set; }
+        public string option2 { get; set; }
+        public object option3 { get; set; }
+        public string created_at { get; set; }
+        public string updated_at { get; set; }
+        public bool taxable { get; set; }
+        public string barcode { get; set; }
+        public object image_id { get; set; }
+        public int inventory_quantity { get; set; }
+        public double weight { get; set; }
+        public string weight_unit { get; set; }
+        public int old_inventory_quantity { get; set; }
+        public bool requires_shipping { get; set; }
+    }
+
+    public class Option
+    {
+        public object id { get; set; }
+        public object product_id { get; set; }
+        public string name { get; set; }
+        public int position { get; set; }
+        public List<string> values { get; set; }
+    }
+
+    public class Image
+    {
+        public object id { get; set; }
+        public object product_id { get; set; }
+        public int position { get; set; }
+        public string created_at { get; set; }
+        public string updated_at { get; set; }
+        public string src { get; set; }
+        public List<object> variant_ids { get; set; }
+    }
+
+    public class Image2
+    {
+        public object id { get; set; }
+        public object product_id { get; set; }
+        public int position { get; set; }
+        public string created_at { get; set; }
+        public string updated_at { get; set; }
+        public string src { get; set; }
+        public List<object> variant_ids { get; set; }
+    }
+
+    public class Product
+    {
+        public object id { get; set; }
+        public string title { get; set; }
+        public string body_html { get; set; }
+        public string vendor { get; set; }
+        public string product_type { get; set; }
+        public string created_at { get; set; }
+        public string handle { get; set; }
+        public string updated_at { get; set; }
+        public string published_at { get; set; }
+        public string template_suffix { get; set; }
+        public string published_scope { get; set; }
+        public string tags { get; set; }
+        public List<Variant> variants { get; set; }
+        public List<Option> options { get; set; }
+        public List<Image> images { get; set; }
+        public Image2 image { get; set; }
+    }
+
+    public class RootObject
+    {
+        public List<Product> products { get; set; }
+    }
+
+}
